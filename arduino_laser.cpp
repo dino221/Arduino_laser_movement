03a13829c8f5c3579d92d43d20e10205e4f58ecf
dino221 <dino.bosilj@gmail.com>
2021-02-06 17:10:37 +0100

    Prvi commit

---
+++ arduino_laser.cpp
@@ -0,0 +1,34 @@
+#include  "Afafruit_VL53L0X.h"
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
+void setup() {
+  Serial.begin(9600);
+  while(! Serial){
+    delay(1);
+  }
+
+  Serial.println("Adafruit VL53L0X test");
+  if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }
+  //power
+  Serial.println(F("VL530X API SimpleRanging example
+  \n\n"));
+}
+
+void loop() {
+  VL53L0X_RangingMeasurementData_t measure;
+
+  Serial.print("Reading a measurement... ");
+  lox.rangingTest(&measure, false);
+
+  if (measure.RangeStatus != 4) {
+    Serial.print("Distance (mm): ");
+    Serial.println(measure.RangeMilliMeter);
+  } else {
+    Serial.println(" out of range ");
+  }
+
+  delay(100);
+}
